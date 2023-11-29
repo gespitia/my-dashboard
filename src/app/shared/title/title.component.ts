a495fd9fb8390f32c64a5be9fe06de904ee48f34
gespitia <gustavoenriqueespitiamelendez@gmail.com>
2023-11-29 12:19:34 -0500

    title component atributos de @Input

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './title.component.html',
+  template: `
+    <h1 class="text-3xl mb-5" >{{title}} - {{withShawdown}}</h1>
+  `,
   styleUrl: './title.component.css'
 })
 export class TitleComponent {
-
+  @Input({required: true}) title!:string;
+  @Input({transform: booleanAttribute}) withShawdown:boolean = false;
 }
