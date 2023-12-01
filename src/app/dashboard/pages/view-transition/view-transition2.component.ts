71d8f1464bd7bbb41cea15338f09a4f0a1987af3
gespitia <gustavoenriqueespitiamelendez@gmail.com>
2023-12-01 15:33:57 -0500

    view-transition, @defer-options, triggers

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="View Transition 2"></app-title>
+    <section class="flex justify-end">
+      <img
+        src=""
+        alt="Picsum"
+        srcset="https://picsum.photos/id/237/200/300"
+        width="200"
+        height="300"
+        style="view-transition-name: hero1"
+      />
+
+      <div class="fixed bg-blue-800 w-32 h-32 rounded bottom-16 raight-10" style="view-transition-name: hero2"></div>
+    </section>
+  `,
+})
+export default class ViewTransitionComponent {}
